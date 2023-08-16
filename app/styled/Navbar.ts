c7722d87bac11b70ffe9869bf99cfd9ea7791ed0
Tsai-Chieh <d0177381@gmail.com>
2023-08-16 09:43:30 +0800

    :bug: should use client and import styled-components without curly brackets, ref from https://github.com/vercel/next.js/issues/46605#issuecomment-1639900755

--- app/styled/Navbar.ts
+++ app/styled/Navbar.ts
@@ -1,4 +1,6 @@
-import { styled } from 'styled-components'
+'use client'
+import styled from 'styled-components'
+
 import { colors } from '../constants/styles'
 
 export const Container = styled.div`
